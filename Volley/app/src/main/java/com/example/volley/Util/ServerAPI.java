5369034ad697fed95d4a471a727fb4d562eb7fbf
budirochmansetyawan16 <66860268+budirochmansetyawan16@users.noreply.github.com>
2021-06-09 19:11:33 +0700

    ApiVolley

---
+++ Volley/app/src/main/java/com/example/volley/Util/ServerAPI.java
@@ -0,0 +1,8 @@
+package com.example.volley.Util;
+
+public class ServerAPI {
+    public static final String URL_DATA = "http://192.168.0.100/smt4/RestApi/ci/Mahasiswa/Api";
+    public static final String URL_INSERT = "http://192.168.0.100/smt4/RestApi/ci/Mahasiswa/ApiInsert";
+    public static final String URL_DELETE = "http://192.168.0.100/smt4/RestApi/ci/Mahasiswa/ApiDelete";
+    public static final String URL_UPDATE = "http://192.168.0.100/smt4/RestApi/ci/Mahasiswa/ApiUpdate";
+}
